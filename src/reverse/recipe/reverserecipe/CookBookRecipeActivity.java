1abd425c4d547895fd49cf34d3ae8514080587f1
kearhs <kawbotting@hotmail.com>
2014-05-21 01:32:48 +1000

    Added ActionBar To CookBook Recipes

--- src/reverse/recipe/reverserecipe/CookBookRecipeActivity.java
+++ src/reverse/recipe/reverserecipe/CookBookRecipeActivity.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -16,7 +17,10 @@ import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,6 +35,7 @@ public class CookBookRecipeActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cookbook_recipe);
+		setupActionBar();
 
 		prefs = getSharedPreferences("reverseRecipe", Context.MODE_PRIVATE);
 
@@ -58,6 +63,48 @@ public class CookBookRecipeActivity extends Activity {
 		setViews();
 
 	}
+	
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_home:
+			Intent homeIntent = new Intent(this,MainActivity.class);
+			startActivity(homeIntent);
+			return true;
+		case R.id.action_cookbook:
+			Intent cookbookIntent = new Intent(this,CookBookActivity.class);
+			startActivity(cookbookIntent);
+			return true;
+		case R.id.action_analytics:
+			Intent analyticsIntent = new Intent(this,AnalyticsActivity.class);
+			startActivity(analyticsIntent);
+			return true;
+		case R.id.action_shopping_list:
+			Intent shoppinglistIntent = new Intent(this,ShoppingListActivity.class);
+			startActivity(shoppinglistIntent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
 
 	public void setViews() {
 
